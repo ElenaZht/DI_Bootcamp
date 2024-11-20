632fd0077def6bf3acf9145ae32272c35b6ca20a
Elena Zht <elenazht@gmail.com>
2024-11-20 15:49:42 +0200

    - day2 exercisesXP exercise8 is done

--- week2/day2/ExercisesXP/exercises8.py
+++ week2/day2/ExercisesXP/exercises8.py
@@ -1,8 +1,16 @@
-# # This project allows users to take a quiz to test their 
-# # Star Wars knowledge.
-# # The number of correct/incorrect answers are tracked 
-# # and the user receives different messages depending on 
-# # how well they did on the quiz.
+# This project allows users to take a quiz to test their 
+# Star Wars knowledge.The number of correct/incorrect answers
+#  are tracked and the user receives different messages 
+# depending on how well they did on the quiz.
+# Create a function that asks the questions to the user, 
+# and check his answers. Track the number of correct, 
+# incorrect answers. Create a list of wrong_answers
+# Create a function that informs the user of his number 
+# of correct/incorrect answers.
+# Bonus : display to the user the questions he answered wrong, 
+# his answer, and the correct answer.
+# If he had more then 3 wrong answers, ask him to play again.
+
 data = [
     {
         "question": "What is Baby Yoda's real name?",
@@ -29,54 +37,33 @@ data = [
         "answer": "Wookiee"
     }
 ]
-# # Create a function that asks the questions to the user, 
-# # and check his answers. Track the number of correct, 
-# # incorrect answers. Create a list of wrong_answers
-# # Create a function that informs the user of his number 
-# # of correct/incorrect answers.
-# # Bonus : display to the user the questions he answered 
-# # wrong, his answer, and the correct answer.
-# # If he had more then 3 wrong answers, ask him to play again.
-# def quizz_results(correct, incorrect, wrong_answers):
-#     if correct == 0:
-#         print("you loose all the quizz")
-#     else:
-#         print(f"you have {correct} right answers!")
 
-#     if incorrect == 0:
-#         print("great job!")
-#     else:
-#         print(f"you have {incorrect} wrong answers")
+def show_info(correct, wrong_answers):
+    print(f"You've got {correct} correct answers and {len(wrong_answers)} incorrect")
+    print("Here your wrong answers:\n")
+    for ans in wrong_answers:
+        question = list(ans.keys())[0]
+        user_answer = list(list(ans.values())[0].values())[0]
+        right_answer = list(list(ans.values())[0].values())[1]
+        print(question)
+        print(f"your answer - {user_answer}, the right - {right_answer}\n")
 
-#     if incorrect > 0:
-#         for wa in wrong_answers.keys():
-#             print(wa)
-#             print(f"answer - {wrong_answers[wa]} is wrong")
-#             print(f"right answer - {data[wa]['answer']}")
+def quizz():
+    correct = 0
+    wrong_answers = []
+    for item in data:
+        user_answer = input(item['question'])
+        if user_answer == item['answer']:
+            correct += 1
+        else:
+            wrong_answers.append({item['question']: {'user_answer': user_answer, 'right_answer':item['answer']}})
+    
+    print('\nYou answers on all the questions!\n')
+    show_info(correct, wrong_answers)
+    if correct <= len(data) - 3:
+        play_again = input('do you want to play again?')
+        if play_again == 'yes':
+            quizz()
 
-# def asker(question_list):
-#     correct = 0
-#     incorrect = 0
-#     wrong_answers = {}
-#     for q in question_list:
-#         answer = input(f"{q['question']}? \n")
-#         if answer == q['answer']:
-#             correct += 1
-#         else:
-#             incorrect += 1
-#             wrong_answers.update({q['question'], q['ap']})
-#     quizz_results(correct, incorrect, wrong_answers)
-#     if incorrect > 3:
-#         if_redo = input("do you want to do it from the start? ")
-#         if if_redo == 'y':
-#             asker(data)
 
-# asker(data)
-
-#
-#def asker():
-#   for q in quas:
-#       user_ans = input("q?")
-#       analizer(q, user_ans)
-# 
-#def analizer()  
+quizz()
