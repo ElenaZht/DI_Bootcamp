3f6f9b7c122307de7dd18e265d111529656b89dd
Elena Zht <elenazht@gmail.com>
2024-12-01 17:03:57 +0200

    -day6 ExercisesXPGold 1 is done

---
+++ week3/day6/ExercisesXPGold/restaurant/menu_editor.py
@@ -0,0 +1,90 @@
+#deal with the UI (user interface), eg. showing the user menu and getting user input, etc.
+#should not have any details about the menu file itself (encapsulation)
+# load_manager()- this function should create a new MenuManager instance.
+from menu_manager import MenuManager
+import string
+
+
+def load_manager():
+    return MenuManager()
+
+# show_user_menu() - this function should display the program menu 
+# (not the restaurant menu!), and ask the user to choose an item. 
+# Call the appropriate function that matches the user’s input.
+def show_user_menu(user_manager):
+    option = ''
+    isChanged = False
+    options = '''
+        MENU
+        (a) Add an item
+        (d) Delete an item
+        (v) View the menu
+        (x) Exit
+'''
+    while option != 'x':
+        print(options)
+        option = input("Pleace, chose option: ")
+        if option == 'a':
+            add_item_to_menu(user_manager)
+        elif option == 'd':
+            remove_item_from_menu(user_manager)
+        elif option == 'v':
+            show_restaurant_menu(user_manager)
+        
+    isChanged = user_manager.save_to_file()
+    if isChanged:
+        print("Changes was saved. Goodbuy!")
+    else:
+        print("No changes to save")
+            
+
+# add_item_to_menu() - this will ask the user to input the item’s name and price. 
+# It will not interact with the menu itself, but simply call the appropriate 
+# function from the MenuManager object.
+# If the item was added successfully print a message which states: 
+# item was added successfully.
+def add_item_to_menu(user_manager):
+    name = input("enter a new posission: ")
+    price = int(input(f"enter {name}'s price: "))
+    result = user_manager.add_item(name, price)
+    if result:
+        print(f"{name} was added successfuly")
+        
+    elif not result:
+        print(f"{name} already exist")
+
+# remove_item_from_menu()- this function should ask the user to input the name 
+# of the item they want to remove from the restaurant’s menu. The function should
+#  not interact with the menu itself, but simply call the appropriate function 
+# from the MenuManager object.
+# If the item was deleted successfully – print a message to let the user know this was completed.
+# If not – print a message which states that there was an error.
+def remove_item_from_menu(user_manager):
+    item = input("enter posission to delete: ").capitalize()
+    return user_manager.remove_item(item)
+    
+# show_restaurant_menu() - print the restaurant’s menu.
+def show_restaurant_menu(user_manager):
+    print("\n *******Menu*******\n")
+    for item in user_manager.menu['items']:
+        print(f"- {item['name']} - {item['price']}$")
+
+# When the user chooses to exit the program, first write the menu 
+# to the file and then print a message that states that the menu was 
+# saved, finally exit the program.
+
+# After exiting the program the changes should be reflected in the menu 
+# (see Part 1.1) and in the JSON file.
+
+# Here’s an example of the menu shown to the user:
+# MENU
+#(a) Add an item
+#(d) Delete an item
+#(v) View the menu
+#(x) Exit
+
+def main():
+    manager = load_manager()
+    show_user_menu(manager)
+
+main()
---
+++ week3/day6/ExercisesXPGold/restaurant/menu_manager.py
@@ -0,0 +1,60 @@
+#handle all other functionality such as adding/removing 
+# items from the menu, along with loading and saving the data to a JSON file.
+import json
+
+
+class MenuManager:
+    def __init__(self):
+        with open('./restaurant_menu.json') as file:
+            self.menu = json.load(file)
+            self.prev_menu = self.menu
+
+    #__init__() - The function should read the menu from the restaurant_menu.json 
+    # file and store it in a variable called menu.
+    def add_item(self, name, price):
+        if not any(item['name'] == name for item in self.menu['items']):
+            self.menu['items'].append({'name': name.lower().capitalize(), 'price': price})
+            print(f"{name} added to menu with price {price}")
+            return True
+        else:
+            print(f"{name} already in the menu")
+            return False
+    #add_item(name, price) – this method should add an item to the menu, although 
+    # do not save the changes to the file yet.
+    def remove_item(self, name):
+        for item in self.menu['items']:
+            if item['name'] == name:
+                self.menu['items'].remove(item)
+                print(f"{name} was deleted successfuly")
+                return True
+        print(f"there is no {name} in menu")
+        return False
+    #remove_item(name) – if the item is in the menu, this function should remove it 
+    # from the menu (and again do not save the changes to the file yet) and return 
+    # True. If the item was not in the menu, return False. (Hint: use Python’s del operator).
+    def save_to_file(self):
+        if self.menu != self.prev_menu:
+            with open('./restaurant_menu.json', 'w') as file:
+                json.dump(self.menu, file, indent=4)
+                return True
+        else: 
+            return False
+        
+    #save_to_file() - When the manager is finished updating the menu this function 
+    # should be called and it should save all the updates and write them into the 
+    # the restaurant_menu.json file (ie. the file which holds the menu).                           
+
+
+def test_menu_manager_methods():
+    m = MenuManager()
+    m.add_item('Cake', 20)
+    m.add_item('Cake', 20) #check for duplicates
+    
+    print(m.remove_item('Cake'))
+    print(m.remove_item('Cake'))#check for double remove
+
+    m.add_item('BD Cake', 25)
+    m.remove_item('Vegetable soup')
+    m.save_to_file()
+
+# test_menu_manager_methods()
