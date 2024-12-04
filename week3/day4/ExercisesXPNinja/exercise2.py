5c1b31f2fbbf59b8bad44dfa5ba6b5ecc635fb02
Elena Zht <elenazht@gmail.com>
2024-12-04 11:48:36 +0200

    - exercise2 entangle method added

--- week3/day4/ExercisesXPNinja/exercise2.py
+++ week3/day4/ExercisesXPNinja/exercise2.py
@@ -1,7 +1,7 @@
 import random
 
 class QuantumParticle:
-    def __init__(self, x, y, p):
+    def __init__(self, x=1000, y=0.1, p=0.5):
         self.x = x
         self.y = y
         self.p = p
@@ -27,6 +27,18 @@ class QuantumParticle:
         self.p = random.choice([1/2, -1/2])
         return self.p
     
+    def entangle(self, second_particle):
+        if not isinstance(second_particle, QuantumParticle):
+            raise Exception("can entangle only with another particle")
+        if self.x == second_particle.x and self.y == second_particle.y and self.p == second_particle.p:
+            print("Spooky Action at a Distance")
+            return False
+        self.disturbance()
+        second_particle.x  = 10000 - self.x
+        second_particle.y = 1 - self.y
+        second_particle.p = self.p * -1
+        print("Particle p1 is now in quantum entanglement with Particle p2")
+    
 
         
     
@@ -40,3 +52,15 @@ print(f"after measurement: x = {particle.x} y = {particle.y} p = {particle.p}\n"
 
 print(f"\nmeasurment spin: {particle.spin()}")
 print(f"after measurement: x = {particle.x} y = {particle.y} p = {particle.p}\n")
+
+particle1 = QuantumParticle(100, 0.5, 0.5)
+particle2 = QuantumParticle(1000, 1, -0.5)
+particle1.entangle(particle2)
+print(f"particle1: {particle1.x} {particle1.y} {particle1.p}")
+print(f"particle2: {particle2.x} {particle2.y} {particle2.p}\n")
+
+particle3 = QuantumParticle()
+particle4 = QuantumParticle()
+particle3.entangle(particle4)
+print(f"particle1: {particle3.x} {particle3.y} {particle3.p}")
+print(f"particle2: {particle4.x} {particle4.y} {particle4.p}\n")
